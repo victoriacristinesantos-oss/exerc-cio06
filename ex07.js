c0b6bea27a89c0af50ecc6eb55c480ef698416f2
Vic.cristines <victoria.cristine.santos@escola.pr.gov.br>
2026-07-09 11:52:16 -0300

    Update ex07.js

--- ex07.js
+++ ex07.js
@@ -1,3 +1,17 @@
 function pegareValores() {
     let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+    return { n1, n2 };
+}
+
+function somar() {
+    let valores = pegarValores();
+    document.getElementById("resultado").innerText = "Resultado: " +
+       (valores.n1 + valores.n2);
+} 
+
+function subtrair() {
+    let valores = pegarValores();
+    document.getElementById("resultado").innerText = "Resultado: " +
+    (valores.n1 - valores.n2);
 }
